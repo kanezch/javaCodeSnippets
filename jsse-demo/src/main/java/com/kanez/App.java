2651a25f1a07b8efaf19c91d1bb95a8b88f57b3d
Kane Zheng <kanez@buildingiq.com>
2020-04-07 23:33:04 +1000

    init

---
+++ jsse-demo/src/main/java/com/kanez/App.java
@@ -0,0 +1,13 @@
+package com.kanez;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
