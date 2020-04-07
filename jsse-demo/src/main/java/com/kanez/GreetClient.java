c78971bb92a2dbc9fb014b6b405c44b129ab1868
Kane Zheng <kanez@buildingiq.com>
2020-04-07 23:56:02 +1000

    server and client without ssl

---
+++ jsse-demo/src/main/java/com/kanez/GreetClient.java
@@ -0,0 +1,35 @@
+package com.kanez;
+
+import java.net.*;
+import java.io.*;
+
+public class GreetClient {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void startConnection(String ip, int port) throws Exception{
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws Exception{
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws Exception{
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public static void main(String[] args) throws Exception{
+        GreetClient client = new GreetClient();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("hello server");
+        System.out.println(response);
+    }
+}
